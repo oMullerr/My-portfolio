a571934972cb4e74363e039fd8c5454f1ee0253a
oMullerr <leindorfmuller@gmail.com>
2022-12-04 01:02:51 -0300

    remove not used comments

--- js/main.js
+++ js/main.js
@@ -245,7 +245,6 @@ function bodyScrollingToggle() {
 
 })();
 
-// aparecer todos as sections menos a ativada
 (() => {
   const sections = document.querySelectorAll(".section");
   sections.forEach((section) => {
@@ -266,23 +265,18 @@ window.addEventListener("load", () => {
 // Get the modal
 let modal = document.getElementById("myModal");
 
-// Get the button that opens the modal
 let btn = document.getElementById("myBtn");
 
-// Get the <span> element that closes the modal
 let span = document.getElementsByClassName("close")[0];
 
-// When the user clicks the button, open the modal 
 btn.onclick = function () {
   modal.style.display = "block";
 }
 
-// When the user clicks on <span> (x), close the modal
 span.onclick = function () {
   modal.style.display = "none";
 }
 
-// When the user clicks anywhere outside of the modal, close it
 window.onclick = function (event) {
   if (event.target == modal) {
     modal.style.display = "none";
