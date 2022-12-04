c8eb419ac7d5c9213c471ded942122f724b9ba3d
oMullerr <leindorfmuller@gmail.com>
2022-12-04 00:15:21 -0300

    add modal

--- js/main.js
+++ js/main.js
@@ -1,34 +1,34 @@
 
 // MENU DE NAVEGAÇÃO 
-(() =>{
+(() => {
   const hamburgerBtn = document.querySelector(".hamburger-btn"),
-  navMenu = document.querySelector(".nav-menu"),
-  closeNavBtn = navMenu.querySelector(".close-nav-menu");
+    navMenu = document.querySelector(".nav-menu"),
+    closeNavBtn = navMenu.querySelector(".close-nav-menu");
 
   hamburgerBtn.addEventListener("click", showNavMenu);
   closeNavBtn.addEventListener("click", hideNavMenu);
 
-  function showNavMenu(){
+  function showNavMenu() {
     navMenu.classList.add("open");
     bodyScrollingToggle();
   }
 
-  function hideNavMenu(){
+  function hideNavMenu() {
     navMenu.classList.remove("open");
     fadeOutEffect();
     bodyScrollingToggle();
   }
 
-  function fadeOutEffect(){
+  function fadeOutEffect() {
     document.querySelector(".fade-out-effect").classList.add("active");
-    setTimeout(() =>{
-      document.querySelector(".fade-out-effect").classList.remove("active");  
-    },300)
+    setTimeout(() => {
+      document.querySelector(".fade-out-effect").classList.remove("active");
+    }, 300)
   }
 
-  document.addEventListener("click", (event) =>{
-    if(event.target.classList.contains('link-item')){
-      if(event.target.hash !==""){
+  document.addEventListener("click", (event) => {
+    if (event.target.classList.contains('link-item')) {
+      if (event.target.hash !== "") {
         event.preventDefault();
         const hash = event.target.hash;
         document.querySelector(".section.active").classList.add("hide");
@@ -38,22 +38,22 @@
         document.querySelector(hash).classList.remove("hide");
 
         // desativando uma section ja active no menu navigation
-        navMenu.querySelector(".active").classList.add("outer-shadow","hover-in-shadow");
-        navMenu.querySelector(".active").classList.remove("active","inner-shadow");
-        if(navMenu.classList.contains("open")){
-          event.target.classList.add("active","inner-shadow");
-          event.target.classList.remove("outer-shadow","hover-in-shadow");
+        navMenu.querySelector(".active").classList.add("outer-shadow", "hover-in-shadow");
+        navMenu.querySelector(".active").classList.remove("active", "inner-shadow");
+        if (navMenu.classList.contains("open")) {
+          event.target.classList.add("active", "inner-shadow");
+          event.target.classList.remove("outer-shadow", "hover-in-shadow");
 
           // hide navigation menu
           hideNavMenu();
         }
-        else{
+        else {
           let navItems = navMenu.querySelectorAll(".link-item");
-          navItems.forEach((item) =>{
-            if(hash === item.hash){
+          navItems.forEach((item) => {
+            if (hash === item.hash) {
               // ativando o menu de navegação
-              item.classList.add("active","inner-shadow");
-              item.classList.remove("outer-shadow","hover-in-shadow");
+              item.classList.add("active", "inner-shadow");
+              item.classList.remove("outer-shadow", "hover-in-shadow");
             }
           })
           fadeOutEffect();
@@ -69,26 +69,26 @@
 
 
 // ABA SOBRE
-(() =>{
-        const aboutSection = document.querySelector(".about-section"),
-        tabsContainer = document.querySelector(".about-tabs");
+(() => {
+  const aboutSection = document.querySelector(".about-section"),
+    tabsContainer = document.querySelector(".about-tabs");
 
-        tabsContainer.addEventListener("click", (event) =>{
-            // 
-            // 
-           if(event.target.classList.contains("tab-item") &&
-             !event.target.classList.contains("active")){
-              const target = event.target.getAttribute("data-target");
-            //   
-              tabsContainer.querySelector(".active").classList.remove("outer-shadow","active");
-            //   
-              event.target.classList.add("active","outer-shadow");
-            // 
-              aboutSection.querySelector(".tab-content.active").classList.remove("active");
-            // 
-              aboutSection.querySelector(target).classList.add("active");
-           }
-        })
+  tabsContainer.addEventListener("click", (event) => {
+    // 
+    // 
+    if (event.target.classList.contains("tab-item") &&
+      !event.target.classList.contains("active")) {
+      const target = event.target.getAttribute("data-target");
+      //   
+      tabsContainer.querySelector(".active").classList.remove("outer-shadow", "active");
+      //   
+      event.target.classList.add("active", "outer-shadow");
+      // 
+      aboutSection.querySelector(".tab-content.active").classList.remove("active");
+      // 
+      aboutSection.querySelector(target).classList.add("active");
+    }
+  })
 })();
 
 function bodyScrollingToggle() {
@@ -96,34 +96,34 @@ function bodyScrollingToggle() {
 }
 
 // FILTRO DO PORFIFOLIO E POPUP
-(() =>{
+(() => {
 
   const filterContainer = document.querySelector(".portfolio-filter"),
-  portfolioItemsContainer = document.querySelector(".portfolio-items"),
-  portfolioItems = document.querySelectorAll(".portfolio-item"),
-  popup = document.querySelector(".portfolio-popup"),
-  prevBtn = popup.querySelector(".pp-prev"),
-  nextBtn = popup.querySelector(".pp-next"),
-  closeBtn = popup.querySelector(".pp-close"),
-  projectDetailsContainer = popup.querySelector(".pp-details"),
-  projectDetailsBtn = popup.querySelector(".pp-project-details-btn");
+    portfolioItemsContainer = document.querySelector(".portfolio-items"),
+    portfolioItems = document.querySelectorAll(".portfolio-item"),
+    popup = document.querySelector(".portfolio-popup"),
+    prevBtn = popup.querySelector(".pp-prev"),
+    nextBtn = popup.querySelector(".pp-next"),
+    closeBtn = popup.querySelector(".pp-close"),
+    projectDetailsContainer = popup.querySelector(".pp-details"),
+    projectDetailsBtn = popup.querySelector(".pp-project-details-btn");
   let itemIndex, slideIndex, screenshots;
 
   // filtro dos itens do portifolio
-  filterContainer.addEventListener("click", (event)=>{
-    if(event.target.classList.contains("filter-item") &&
-    !event.target.classList.contains("active")){
+  filterContainer.addEventListener("click", (event) => {
+    if (event.target.classList.contains("filter-item") &&
+      !event.target.classList.contains("active")) {
       // caso tenha algum botao ativado
-      filterContainer.querySelector(".active").classList.remove("outer-shadow","active");
+      filterContainer.querySelector(".active").classList.remove("outer-shadow", "active");
       // ativando outro botao
-      event.target.classList.add("active","outer-shadow");
+      event.target.classList.add("active", "outer-shadow");
       const target = event.target.getAttribute("data-target");
-      portfolioItems.forEach((item)=>{
-        if(target === item.getAttribute("data-category") || target === 'all'){
+      portfolioItems.forEach((item) => {
+        if (target === item.getAttribute("data-category") || target === 'all') {
           item.classList.remove("hide");
           item.classList.add("show");
         }
-        else{
+        else {
           item.classList.remove("show");
           item.classList.add("hide");
         }
@@ -131,21 +131,21 @@ function bodyScrollingToggle() {
     }
   })
 
-  portfolioItemsContainer.addEventListener("click", (event) =>{
-    if(event.target.closest(".portfolio-item-inner")){
+  portfolioItemsContainer.addEventListener("click", (event) => {
+    if (event.target.closest(".portfolio-item-inner")) {
       const portfolioItem = event.target.closest(".portfolio-item-inner").parentElement;
       // pegar o index do portfolioItem
       itemIndex = Array.from(portfolioItem.parentElement.children).indexOf(portfolioItem);
       screenshots = portfolioItems[itemIndex].querySelector(".portfolio-item-img img").getAttribute("data-screenshots");
       // 
       screenshots = screenshots.split(",");
-      if(screenshots.length === 1){
-        prevBtn.style.display="none";
-        nextBtn.style.display="none";
+      if (screenshots.length === 1) {
+        prevBtn.style.display = "none";
+        nextBtn.style.display = "none";
       }
-      else{
-        prevBtn.style.display="block";
-        nextBtn.style.display="block";
+      else {
+        prevBtn.style.display = "block";
+        nextBtn.style.display = "block";
       }
 
       slideIndex = 0;
@@ -155,9 +155,9 @@ function bodyScrollingToggle() {
     }
   })
 
-  closeBtn.addEventListener("click", () =>{
+  closeBtn.addEventListener("click", () => {
     popupToogle();
-    if(projectDetailsContainer.classList.contains("active")){
+    if (projectDetailsContainer.classList.contains("active")) {
       popupDetailsToggle();
     }
   })
@@ -167,47 +167,47 @@ function bodyScrollingToggle() {
     bodyScrollingToggle();
   }
 
-  function popupSlideshow(){
+  function popupSlideshow() {
     const imgSrc = screenshots[slideIndex];
     const popupImg = popup.querySelector(".pp-img");
     //
     popup.querySelector(".pp-loader").classList.add("active");
-    popupImg.src=imgSrc;
-    popupImg.onload = () =>{
+    popupImg.src = imgSrc;
+    popupImg.onload = () => {
       // desativando o popup depois da imagem ser carregada
       popup.querySelector(".pp-loader").classList.remove("active");
     }
-    popup.querySelector(".pp-counter").innerHTML = (slideIndex+1) + " DE " + screenshots.length;
+    popup.querySelector(".pp-counter").innerHTML = (slideIndex + 1) + " DE " + screenshots.length;
 
   }
   // proxima foto
-  nextBtn.addEventListener("click", () =>{
-    if(slideIndex === screenshots.length-1){
+  nextBtn.addEventListener("click", () => {
+    if (slideIndex === screenshots.length - 1) {
       slideIndex = 0;
     }
-    else{
+    else {
       slideIndex++;
     }
     popupSlideshow();
   })
 
-  prevBtn.addEventListener("click", () =>{
-    if(slideIndex === 0){
-      slideIndex = screenshots.length-1
+  prevBtn.addEventListener("click", () => {
+    if (slideIndex === 0) {
+      slideIndex = screenshots.length - 1
     }
-    else{
+    else {
       slideIndex--;
     }
     popupSlideshow();
   })
 
-  function popupDetails(){
+  function popupDetails() {
     // se o projeto nao tem detalhes
-    if(!portfolioItems[itemIndex].querySelector(".portfolio-item-details")){
-      projectDetailsBtn.style.display="none";
+    if (!portfolioItems[itemIndex].querySelector(".portfolio-item-details")) {
+      projectDetailsBtn.style.display = "none";
       return; // final da função
     }
-    projectDetailsBtn.style.display="block";
+    projectDetailsBtn.style.display = "block";
 
     // pegar os detalhes dos projetos
     const details = portfolioItems[itemIndex].querySelector(".portfolio-item-details").innerHTML;
@@ -223,42 +223,68 @@ function bodyScrollingToggle() {
     popup.querySelector(".pp-project-category").innerHTML = category.split("-").join(" ");
   }
 
-  projectDetailsBtn.addEventListener("click", () =>{
+  projectDetailsBtn.addEventListener("click", () => {
     popupDetailsToggle();
   })
 
-  function popupDetailsToggle(){
-    if(projectDetailsContainer.classList.contains("active")){
+  function popupDetailsToggle() {
+    if (projectDetailsContainer.classList.contains("active")) {
       projectDetailsBtn.querySelector("i").classList.remove("fa-minus");
       projectDetailsBtn.querySelector("i").classList.add("fa-plus");
       projectDetailsContainer.classList.remove("active");
       projectDetailsContainer.style.maxHeight = 0 + "px";
     }
-    else{
+    else {
       projectDetailsBtn.querySelector("i").classList.remove("fa-plus");
       projectDetailsBtn.querySelector("i").classList.add("fa-minus");
       projectDetailsContainer.classList.add("active");
       projectDetailsContainer.style.maxHeight = projectDetailsContainer.scrollHeight + "px";
-      popup.scrollTo(0,projectDetailsContainer.offsetTop);
+      popup.scrollTo(0, projectDetailsContainer.offsetTop);
     }
   }
 
 })();
 
 // aparecer todos as sections menos a ativada
-(() =>{
+(() => {
   const sections = document.querySelectorAll(".section");
-  sections.forEach((section) =>{
-    if(!section.classList.contains("active")){
+  sections.forEach((section) => {
+    if (!section.classList.contains("active")) {
       section.classList.add("hide");
     }
   })
 })();
 
-window.addEventListener("load", () =>{
+window.addEventListener("load", () => {
   // preloader
   document.querySelector(".preloader").classList.add("fade-out");
-  setTimeout(() =>{
-    document.querySelector(".preloader").style.display="none";
-  },600)
+  setTimeout(() => {
+    document.querySelector(".preloader").style.display = "none";
+  }, 600)
 })
+
+// Get the modal
+var modal = document.getElementById("myModal");
+
+// Get the button that opens the modal
+var btn = document.getElementById("myBtn");
+
+// Get the <span> element that closes the modal
+var span = document.getElementsByClassName("close")[0];
+
+// When the user clicks the button, open the modal 
+btn.onclick = function () {
+  modal.style.display = "block";
+}
+
+// When the user clicks on <span> (x), close the modal
+span.onclick = function () {
+  modal.style.display = "none";
+}
+
+// When the user clicks anywhere outside of the modal, close it
+window.onclick = function (event) {
+  if (event.target == modal) {
+    modal.style.display = "none";
+  }
+}
